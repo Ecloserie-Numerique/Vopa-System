776d9d5125200475e2d631270b1cde16ed449541
aymux <malezieux.aymeric@gmail.com>
2020-06-20 14:50:20 +0200

    Vopa v1.0

--- vopa-install.sh
+++ vopa-install.sh
@@ -29,7 +29,7 @@ read -p "Press enter to continue"
 echo "┌─────────────────────────────────────────"
 echo "|Updating repositories"
 echo "└─────────────────────────────────────────"
-apt-get update -yqq
+apt update -y
 
 # echo "┌─────────────────────────────────────────"
 # echo "|Upgrading packages, this might take a while|"
@@ -39,24 +39,23 @@ apt-get update -yqq
 echo "┌─────────────────────────────────────────"
 echo "|Installing and configuring nginx"
 echo "└─────────────────────────────────────────"
-apt-get install nginx -yqq
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/default_nginx -O /etc/nginx/sites-enabled/default
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/index.php -O  /var/www/html/index.php
+apt install nginx -y
+wget -q https://raw.githubusercontent.com/Ecloserie-Numerique/Vopa-System/master/default_nginx -O /etc/nginx/sites-enabled/default
 
 echo "┌─────────────────────────────────────────"
 echo "|Installing dnsmasq"
 echo "└─────────────────────────────────────────"
-apt-get install dnsmasq -yqq
+apt install dnsmasq -y
 
 echo "┌─────────────────────────────────────────"
 echo "|Configuring wlan0"
 echo "└─────────────────────────────────────────"
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/dhcpcd.conf -O /etc/dhcpcd.conf
+wget -q https://raw.githubusercontent.com/Ecloserie-Numerique/Vopa-System/master/dhcpcd.conf -O /etc/dhcpcd.conf
 
 echo "┌─────────────────────────────────────────"
 echo "|Configuring dnsmasq"
 echo "└─────────────────────────────────────────"
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/dnsmasq.conf -O /etc/dnsmasq.conf
+wget -q https://raw.githubusercontent.com/Ecloserie-Numerique/Vopa-System/master/dnsmasq.conf -O /etc/dnsmasq.conf
 
 echo "┌─────────────────────────────────────────"
 echo "|configuring dnsmasq to start at boot"
@@ -66,14 +65,14 @@ update-rc.d dnsmasq defaults
 echo "┌─────────────────────────────────────────"
 echo "|Installing hostapd"
 echo "└─────────────────────────────────────────"
-apt-get install hostapd -yqq
+apt install hostapd -y
 
 echo "┌─────────────────────────────────────────"
 echo "|Configuring hostapd"
 echo "└─────────────────────────────────────────"
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/hostapd.conf -O /etc/hostapd/hostapd.conf
+wget -q https://raw.githubusercontent.com/Ecloserie-Numerique/Vopa-System/master/hostapd.conf -O /etc/hostapd/hostapd.conf
 sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
-sed -i -- "s/CaptivePortal01/${SSID}/g" /etc/hostapd/hostapd.conf
+sed -i -- "s/VOPA LOCAL/${SSID}/g" /etc/hostapd/hostapd.conf
 
 echo "┌─────────────────────────────────────────"
 echo "|Configuring iptables"
@@ -82,10 +81,10 @@ iptables -t nat -A PREROUTING -s 192.168.24.0/24 -p tcp --dport 80 -j DNAT --to-
 iptables -t nat -A POSTROUTING -j MASQUERADE
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-apt-get -y install iptables-persistent
+apt -y install iptables-persistent
 
 echo "┌─────────────────────────────────────────"
-echo "|configuring hostapd to start at boot"
+echo "|Configuring hostapd to start at boot"
 echo "└─────────────────────────────────────────"
 systemctl unmask hostapd.service
 systemctl enable hostapd.service
@@ -93,7 +92,29 @@ systemctl enable hostapd.service
 echo "┌─────────────────────────────────────────"
 echo "|Installing PHP7"
 echo "└─────────────────────────────────────────"
-apt-get install php7.3-fpm php7.3-mbstring php7.3-mysql php7.3-curl php7.3-gd php7.3-curl php7.3-zip php7.3-xml -yqq > /dev/null
+apt install php7.3-fpm php7.3-mbstring php7.3-mysql php7.3-curl php7.3-gd php7.3-curl php7.3-zip php7.3-xml -y > /dev/null
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing NODE.JS"
+echo "└─────────────────────────────────────────"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt install nodejs -y
+
+echo "┌─────────────────────────────────────────"
+echo "|Copying vopa files"
+echo "└─────────────────────────────────────────"
+wget -q https://raw.githubusercontent.com/Ecloserie-Numerique/Vopa-System/master/vopa-client.tar.gz -O /var/www
+wget -q https://raw.githubusercontent.com/Ecloserie-Numerique/Vopa-System/master/vopa-server.tar.gz -O /var/www
+tar -zxvf vopa-client.tar.gz
+tar -zxvf vopa-server.tar.gz
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring Vopa Server Service"
+echo "└─────────────────────────────────────────"
+wget -q https://raw.githubusercontent.com/Ecloserie-Numerique/Vopa-System/master/server.service -O /lib/systemd/system
+systemctl daemon-reload
+systemctl enable server
+
 
 echo "┌─────────────────────────────────────────"
 echo "|Please reboot your pi and test."
